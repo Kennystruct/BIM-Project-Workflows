c26a284fcf04bc2f1b1e1ed71af8cc8987658e80
Kehinde Ayobade <kehindeayobade@gmail.com>
2021-09-15 14:50:30 +0100

    Update Merge Image to PDF.py

--- SCRIPTS/Merge Image to PDF.py
+++ SCRIPTS/Merge Image to PDF.py
@@ -1,31 +1,39 @@
+'''This script helps you merge any specified number of images inside a folder into
+a single PDF file without the need to download any third party software'''
+
+#IMPORT THE REQUIRED PYTHON LIBRARIES FOR THIS WORKFLOW
 import fpdf
 import os
 from PIL import Image
 from fpdf import FPDF
 
 pdf = FPDF()
+
+#SPECIFY THE NAME OF THE OUTPUT PDF FILE
 PDF_Name = input("PDF file name:") + ".pdf"
 
-#BIM for Structural Engineers - Start-Up Guide - 0.pdf"
-
+#SPECIFY A GENERAL WIDTH AND HEIGHT OF THE IMAGES IN PIXELS
 W_pixels = 1000
 H_pixels = 1000
 
+#THIS LINE OF CODE CONVERTS THE SIZE OF THE IMAGES FROM PIXELS TO MILLIMETERS
 Pixel_MM_Converter = 0.2645833333
 w = Pixel_MM_Converter * W_pixels
 h = Pixel_MM_Converter * H_pixels
 
+#SPECIFY THE FILE DIRECTORY WHERE THE IMAGES ARE LOCATED
 Files_Path = r'C:\Users\m\Dropbox\KS DIGITAL INNOVATIONS\05 - SOCIAL MEDIA CONTENTS\01 - BIM 4 STRUCTURAL ENGINEERS'
 
 os.chdir(Files_Path)
 
+#SPECIFY THE IMAGE NAME IN AN ARRAY OF NUMBERS
 imageName = r"\POST {}.png"
 
-
+#CHECK IF FILE DIRECTORY EXISTS
 os.path.exists(Files_Path)
 
 output = []
-Total_Files = 12 #This is the total number of files in the directory
+Total_Files = 12 #Specify the number of image files in the directory you want to merge into PDF
 
 for n in range(1,Total_Files+1):
     fname = Files_Path + imageName.format(n)
